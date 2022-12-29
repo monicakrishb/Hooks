c3ae1488683323ce4dbf3a1b4bedd7510a023016
monicakrishb <monica.krish@aspiresys.com>
2022-12-29 17:38:39 +0530

    modified count

--- hooks/src/ParentComponent.js
+++ hooks/src/ParentComponent.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import Count from './Count'
 import Button from './Button'
 import Title from './Title'
 
-export default function ParentComponent() {
+ function ParentComponent() {
     const[age,setAge]=useState(25)
     const[salary,setSalary]=useState(50000)
 
-    const incrementAge=()=>{
-        setAge(age+1)
-    }
-    const incrementSalary=()=>{
+    const incrementAge=useCallback(()=>{
+      setAge(age+1)
+  },[age])
+    const incrementSalary=useCallback(()=>{
         setSalary(salary+1000)
-    }
+    },[salary])
   return (
     <div>
         <Title/>
@@ -23,3 +23,4 @@ export default function ParentComponent() {
     </div>
   )
 }
+export default ParentComponent
